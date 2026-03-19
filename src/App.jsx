49cb8cbb8418f2b651e1867648070854a0506250
Palesa Malatshi <134342703+palesamalatshi@users.noreply.github.com>
2026-03-19 14:17:46 +0200

    feat: configure routing and global state with PodcastProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import ShowDetail from "./pages/ShowDetail"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/UI/Header";
+import Home from "./pages/Home";
+import ShowDetail from "./pages/ShowDetail";
+import { PodcastProvider } from "./context/PodcastContext";
 
+/**
+ * Root component of the Podcast Explorer app.
+ */
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-
-        <Route path="/show/:id" element={<ShowDetail />} />
-
-      </Routes>
+      <PodcastProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/show/:id" element={<ShowDetail />} />
+        </Routes>
+      </PodcastProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
